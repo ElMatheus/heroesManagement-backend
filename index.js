03b446c1535392cb728c22a23fe46322c5864104
pravel <123407140+ElMatheus@users.noreply.github.com>
2024-05-06 09:43:32 -0300

    feat: CRUD heroes

--- index.js
+++ index.js
@@ -18,20 +18,62 @@ app.get('/', (req, res) => {
 });
 
 app.get('/heroes', async (req, res) => {
-    const result = await pool.query('SELECT * FROM heroes');
-    res.json({
-        total: result.rowCount,
-        heroes: result.rows,
-    });
+    try {
+        const result = await pool.query('SELECT * FROM heroes');
+        res.json({
+            total: result.rowCount,
+            heroes: result.rows,
+        });
+    } catch (error) {
+        console.error('Erro ao buscar herois', error);
+        res.status(500).send('Erro ao buscar herois');
+    }
 });
 
 app.get('/heroes/:id', async (req, res) => {
-    const { id } = req.params;
-    const result = await pool.query('SELECT * FROM heroes WHERE id = $1', [id]);
-    if(result.rowCount === 0) {
-        res.status(404).json({ message: 'Heroi não encontrado' });
+    try {
+        const { id } = req.params;
+        const result = await pool.query('SELECT * FROM heroes WHERE id = $1', [id]);
+        if (result.rowCount === 0) {
+            res.status(404).json({ message: 'Heroi não encontrado' });
+        }
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error('Erro ao buscar heroi', error);
+        res.status(500).send('Erro ao buscar heroi');
     }
-    res.json(result.rows[0]);
+});
+
+app.post('/heroes', async (req, res) => {
+    try {
+        const { name, power, hp, attack } = req.body;
+        const result = await pool.query('INSERT INTO heroes (name, level, power, hp, attack) VALUES ($1, $2, $3, $4, $5) RETURNING *', [name, 1, power, hp, attack]);
+        res.json({
+            message: "Heroi cadastrado com sucesso",
+            users: result.rows[0],
+        });
+    } catch (error) {
+        console.error('Erro ao criar herois', error);
+        res.status(500).send('Erro ao criar herois');
+    }
+});
+
+app.put('/heroes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, power, hp, attack } = req.body;
+        const result = await pool.query('UPDATE heroes SET name = $1, power = $2, hp = $3, attack = $4 WHERE id = $5 RETURNING *', [name, power, hp, attack, id]);
+        if (result.rowCount === 0) {
+            res.status(404).json({ message: 'Heroi não encontrado' });
+        }
+        res.json({
+            message: "Heroi atualizado com sucesso",
+            users: result.rows[0],
+        });
+    } catch (error) {
+        console.error('Erro ao atualizar heroi', error);
+        res.status(500).send('Erro ao atualizar heroi');
+    }
 });
 
 app.listen(PORT, () => {
