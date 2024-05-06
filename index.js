9ca4aa36cecf04a6fa0d9112c798577a630c5238
pravel <123407140+ElMatheus@users.noreply.github.com>
2024-05-06 11:04:08 -0300

    feat: battle function

--- index.js
+++ index.js
@@ -90,7 +90,61 @@ app.delete('/heroes/:id', async (req, res) => {
     }
 });
 
+// battles routes
 
+const battle = (hero1, hero2) => {
+    const hero1Attack = hero1.attack;
+    const hero2Attack = hero2.attack;
+    const hero1Hp = hero1.hp;
+    const hero2Hp = hero2.hp;
+    var hero1CountAtacks = 0;
+    var hero2CountAtacks = 0;
+    let hero1Turn = true;
+    let hero1CurrentHp = hero1Hp;
+    let hero2CurrentHp = hero2Hp;
+    while (hero1CurrentHp > 0 && hero2CurrentHp > 0) {
+        if (hero1Turn) {
+            hero2CurrentHp -= hero1Attack;
+            hero1CountAtacks++;
+            console.log(hero1.name + ' atacou e tirou ' + hero1Attack + ' de vida do ' + hero2.name + ' deixando com ' + hero2CurrentHp + ' de vida restante');
+        } else {
+            hero1CurrentHp -= hero2Attack;
+            hero2CountAtacks++;
+            console.log(hero2.name + ' atacou e tirou ' + hero2Attack + ' de vida do ' + hero1.name + ' deixando com ' + hero1CurrentHp + ' de vida restante');
+        }
+        hero1Turn = !hero1Turn;
+    }
+    if (hero1CurrentHp > 0) {
+        return { hero: hero1, countAtacks: hero1CountAtacks };
+    } else {
+        return { hero: hero2, countAtacks: hero2CountAtacks };
+    }
+};
+
+app.get('/battles/:heroi1/:heroi2', async (req, res) => {
+    try {
+        const { heroi1, heroi2 } = req.params;
+        const hero1 = await pool.query('SELECT * FROM heroes WHERE id = $1', [heroi1]);
+        const hero2 = await pool.query('SELECT * FROM heroes WHERE id = $1', [heroi2]);
+        if (hero1.rowCount === 0 || hero2.rowCount === 0) {
+            res.status(404).json({ message: 'Heroi não encontrado' });
+        } else {
+            const winner = battle(hero1.rows[0], hero2.rows[0]);
+            res.json({
+                winner: winner.hero,
+                golpes: winner.countAtacks,
+                message: 'Batalha finalizada',
+            });
+        }
+
+
+
+
+    } catch (error) {
+        console.error('Erro ao buscar batalha', error);
+        res.status(500).send('Erro ao buscar batalha');
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`O servidor esta rodando na porta ${PORT}`);
