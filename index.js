a1abd07e4ff0f3a044c401f3ff36a5c0b95afd52
pravel <123407140+ElMatheus@users.noreply.github.com>
2024-05-06 09:25:43 -0300

    feat: rotes heroes

--- index.js
+++ index.js
@@ -3,10 +3,35 @@ const { Pool } = require('pg');
 const app = express();
 const PORT = 3000;
 
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'heroes',
+    password: 'ds564',
+    port: 5432,
+});
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Rota teste funfando');
+});
+
+app.get('/heroes', async (req, res) => {
+    const result = await pool.query('SELECT * FROM heroes');
+    res.json({
+        total: result.rowCount,
+        heroes: result.rows,
+    });
+});
+
+app.get('/heroes/:id', async (req, res) => {
+    const { id } = req.params;
+    const result = await pool.query('SELECT * FROM heroes WHERE id = $1', [id]);
+    if(result.rowCount === 0) {
+        res.status(404).json({ message: 'Heroi não encontrado' });
+    }
+    res.json(result.rows[0]);
 });
 
 app.listen(PORT, () => {
